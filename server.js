5e802c6638fc95ed9a41a7d48d12ad0a6d515bf3
unknown <pillorajem10@gmail.com>
2025-07-25 14:36:22 +0800

    made adjustments in server

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const port = process.env.PORT || 4000;
 const server = http.createServer(app);
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
 // Routes
